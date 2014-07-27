d9ddb56b940cdd90f4917caa5bdaf9bfeafd0b05
Bo Jeanes <me@bjeanes.com>
2014-07-26 19:18:18 -0700

    Start microservice for providing a place to stick source blobs

---
+++ web/README.md
@@ -0,0 +1,30 @@
+# hk-deploy web
+
+Eventually, a small HTTP that serves out short-lived policies for uploading a
+single file to an S3 bucket and a short-lived URL for accessing that file.
+
+It will be used by the `hk` plugin at the root of this repository (`hk-deploy`)
+to upload a directory of source code as a `.tgz` file so the Heroku Build API
+can [create a build using the source](https://devcenter.heroku.com/articles/build-and-release-using-the-api#creating-builds).
+
+The bucket that stores this code has the minimum allowable life cycle (24 hours)
+for automatically deleting files. It only accepts `.tgz` files that are less than
+200 MB\*. The bucket and all items are set to private so the URL provided by this
+service is the only way to retrieve the file and it will only work for 30 minutes.
+
+\* restriction not currently implemented
+
+## Testing
+
+``` sh-session
+$ hk env -a hk-deploy | xargs -J __ env __ go run main.go curl
+curl https://hk-deploy.s3.amazonaws.com/ \
+  -Fkey="20140727/9c5ba3ed-91fe-4ab3-9c4a-705b3dab9bb4.tgz" \
+  -Facl=private -FContent-Type=application/x-compressed \
+  -FX-Amz-Credential="AKIAI73CN3SXNGBBBYEA/20140727/us-east-1/s3/aws4_request" \
+  -FX-Amz-Algorithm=AWS4-HMAC-SHA256 \
+  -FX-Amz-Date="20140727T020425Z" \
+  -FPolicy="ewoJImV4cGlyYXRpb24iOiAiMjAxNC0wNy0yN1QwMjoxNDoyNVoiLAoJImNvbmRpdGlvbnMiOiBbCgkJeyJhY2wiOiAicHJpdmF0ZSIgfSwKCQl7ImJ1Y2tldCI6ICJoay1kZXBsb3kiIH0sCgkJeyJrZXkiOiAiMjAxNDA3MjcvOWM1YmEzZWQtOTFmZS00YWIzLTljNGEtNzA1YjNkYWI5YmI0LnRneiJ9LAoJCXsieC1hbXotZGF0ZSI6ICIyMDE0MDcyN1QwMjA0MjVaIn0sCgkJeyJ4LWFtei1jcmVkZW50aWFsIjogIkFLSUFJNzNDTjNTWE5HQkJCWUVBLzIwMTQwNzI3L3VzLWVhc3QtMS9zMy9hd3M0X3JlcXVlc3QifSwKCQl7IngtYW16LWFsZ29yaXRobSI6ICJBV1M0LUhNQUMtU0hBMjU2In0sCgkJeyJjb250ZW50LXR5cGUiOiAiYXBwbGljYXRpb24veC1jb21wcmVzc2VkIn0KCV0KfQ==" \
+  -FX-Amz-Signature="fc32665b200bfbbf02cc8a46ae3d32ff9923242ee2d549f79eb14b6db15db43e" \
+  -Ffile=@some-tarred-code.tgz
+```
---
+++ web/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	// s3 "github.com/mitchellh/goamz/s3"
+	"code.google.com/p/go-uuid/uuid"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"time"
+)
+
+var (
+	bucket = os.Getenv("AWS_S3_BUCKET")
+	key_id = os.Getenv("AWS_ACCESS_KEY_ID")
+	secret = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	action = "https://" + bucket + ".s3.amazonaws.com/"
+	expire = 10 * time.Minute
+)
+
+const (
+	region = "us-east-1" // FIXME: can be derived from bucket?
+
+	// time layouts based on reference time (see pkg "time")
+	iso8601 = "20060102T150405Z0700"
+	short   = "20060102"
+)
+
+func sign(key, data []byte) []byte {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(data)
+	return mac.Sum(nil)
+}
+
+func creds(now time.Time) (credential string, signingKey []byte) {
+	credential = key_id + "/" + now.Format(short) + "/" + region + "/s3/aws4_request"
+	signingKey = []byte("AWS4" + secret)
+	signingKey = sign(signingKey, []byte(now.Format(short)))
+	signingKey = sign(signingKey, []byte(region))
+	signingKey = sign(signingKey, []byte("s3"))
+	signingKey = sign(signingKey, []byte("aws4_request"))
+
+	return
+}
+
+func main() {
+	now := time.Now().UTC()
+	isoNow := now.Format(iso8601)
+	expiry := now.Add(expire).Format(time.RFC3339)
+	key := now.Format(short) + "/" + uuid.New() + ".tgz"
+
+	credential, signingKey := creds(now)
+
+	policy := `{
+	"expiration": "` + expiry + `",
+	"conditions": [
+		{"acl": "private" },
+		{"bucket": "` + bucket + `" },
+		{"key": "` + key + `"},
+		{"x-amz-date": "` + isoNow + `"},
+		{"x-amz-credential": "` + credential + `"},
+		{"x-amz-algorithm": "AWS4-HMAC-SHA256"},
+		{"content-type": "application/x-compressed"}
+	]
+}`
+
+	encodedPolicy := base64.StdEncoding.EncodeToString([]byte(policy))
+	signature := hex.EncodeToString(sign(signingKey, []byte(encodedPolicy)))
+
+	var response string
+
+	if false { // let me switch to and from a curl command I can just run for testing...
+		response = `{
+	"action": "` + action + `",
+	"fields": {
+		"key": "` + key + `",
+		"acl": "private",
+		"Content-Type": "application/x-compressed",
+		"X-Amz-Credential": "` + credential + `",
+		"X-Amz-Algorithm": "AWS4-HMAC-SHA256",
+		"X-Amz-Date": "` + isoNow + `",
+		"Policy": "` + encodedPolicy + `",
+		"X-Amz-Signature": "` + signature + `"
+	}
+}`
+	} else {
+		response = `curl ` + action + ` ` +
+			`-Fkey="` + key + `" ` +
+			`-Facl=private ` +
+			`-FContent-Type=application/x-compressed ` +
+			`-FX-Amz-Credential="` + credential + `" ` +
+			`-FX-Amz-Algorithm=AWS4-HMAC-SHA256 ` +
+			`-FX-Amz-Date="` + isoNow + `" ` +
+			`-FPolicy="` + encodedPolicy + `" ` +
+			`-FX-Amz-Signature="` + signature + `" ` +
+			`-Ffile=@foo.tgz`
+
+	}
+
+	fmt.Println(response)
+}
